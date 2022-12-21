6b2ae547d464ec21929da771a6d6644750a50f8d
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-12-21 16:27:22 +0100

    starter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className='flex justify-center items-center max-w-6xl mx-auto'>
+      <h1 className='text-3xl my-8'>Hello Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
